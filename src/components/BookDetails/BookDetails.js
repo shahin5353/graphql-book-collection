dc40f79eb8d7bbd7209d60ea008d23af90f0bb2c
Shahin <ahmshahin5353@gmail.com>
2020-07-04 17:26:47 +0600

    style added

--- src/components/BookDetails/BookDetails.js
+++ src/components/BookDetails/BookDetails.js
@@ -3,34 +3,34 @@ import { graphql } from 'react-apollo';
 import { getBookQuery } from '../../queries/queries';
 
 const BookDetails = (props) => {
-    const {book} = props.data;
+    const { book } = props.data;
     return (
         <>
-          {
-              book ?
-              <div>
-              <h2 className="text-capitalize">{book.name}</h2>
-              <p className="font-weight-light">{book.genre}</p>
-          <p className="font-italic mb-0">{book.author.name}</p>
-          <p className="lead">All books by this author</p>
-          <ul className="other-books">
-              {book.author.books.map(item=>{
-                  return <li className="text-capitalize" key={item.id}>{item.name}</li>
-              })}
-          </ul>
-              </div>
-              :
-              <div>No book Selected...</div>
-          }
-          
+            {
+                book ?
+                    <div>
+                        <h2 className="text-capitalize">{book.name}</h2>
+                        <p className="font-italic mb-0">{book.genre}</p>
+                        <p className="font-weight-light">{book.author.name}</p>
+                        <p className="lead">All books by this author</p>
+                        <ul className="other-books">
+                            {book.author.books.map(item => {
+                                return <li className="text-capitalize" key={item.id}>{item.name}</li>
+                            })}
+                        </ul>
+                    </div>
+                    :
+                    <div>No book Selected...</div>
+            }
+
         </>
     );
 };
 
-export default graphql(getBookQuery,{
-    options:(props)=>{
+export default graphql(getBookQuery, {
+    options: (props) => {
         return {
-            variables:{
+            variables: {
                 id: props.bookId
             }
         }
